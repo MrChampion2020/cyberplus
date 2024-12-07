add49033a2ac0ef38548a6c774292cc69e966613
Champion Aden <65666205+MrChampion2020@users.noreply.github.com>
2024-12-07 10:10:18 +0100

    Delete api directory

--- api/Routes/auth.js
+++
@@ -1,8 +0,0 @@
-// import  express  from "express";
-// import { register, login, adminLogin } from "../Controllers/authController.js";
-
-// const router=express.Router()
-// router.post('/register',register)
-// router.post('/login',login)
-// router.post('/adminLogin',adminLogin)
-// export default router
